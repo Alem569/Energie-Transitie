98f4f9bc2648194a2472b9c17b54ae9abf434ed5
Alem <106407795+Alem569@users.noreply.github.com>
2025-06-18 15:05:46 +0200

    add: widgets for locaties

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { Locations } from '@/components/ui/locations';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -14,8 +15,11 @@ export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="flex h-full flex-1 flex-col gap-5 rounded-xl p-4 overflow-x-auto">
+                <div className="grid auto-rows-min gap-40 md:grid-cols-4">
+                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
@@ -26,8 +30,31 @@ export default function Dashboard() {
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                <div className="grid auto-rows-min gap-10 md:grid-cols-3">
+                    <div
+                        onClick={() => console.log('Clicked Amsterdam Noord')}
+                        className="cursor-pointer relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border transition-transform duration-300 ease-in-out hover:scale-102"
+                    >
+                        <Locations locationName="Amsterdam Noord" />
+                    </div>
+                    <div
+                        onClick={() => console.log('Clicked Waddinxveen')}
+                        className="cursor-pointer relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border transition-transform duration-300 ease-in-out hover:scale-102"
+                    >
+                        <Locations locationName="Waddinxveen" />
+                    </div>
+                    <div
+                        onClick={() => console.log('Clicked Rotterdam West')}
+                        className="cursor-pointer relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border transition-transform duration-300 ease-in-out hover:scale-102"
+                    >
+                        <Locations locationName="Rotterdam West" />
+                    </div>
+                    <div
+                        onClick={() => console.log('Clicked Gouda')}
+                        className="cursor-pointer relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border transition-transform duration-300 ease-in-out hover:scale-102"
+                    >
+                        <Locations locationName="Gouda" />
+                    </div>  
                 </div>
             </div>
         </AppLayout>
