f38cd0dabf425707207392d9077bf8143890b026
strahinjazoranovic <6042877@mborijnland.nl>
2025-06-25 14:33:37 +0200

    V0.17
    Adjusted padding and gap values in AuthSimpleLayout and login form for better spacing. Enhanced the welcome page with improved responsive styles, centering, and button sizing for a more consistent appearance across devices.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -2,13 +2,22 @@ export default function Welcome() {
     return (
         <div className="relative h-screen w-full">
             <img src="/energy.png" alt="Energy" className="absolute inset-0 z-0 h-full w-full object-cover brightness-90" />
-            <div className="font-medium absolute top-3/5 left-18 z-10 space-y-4 text-7xl text-white">
+            <div
+                className="
+                    font-medium absolute z-10 space-y-5 text-4xl md:text-7xl text-white
+                    left-1/2 top-1/2 md:left-14 md:top-[60%]
+                    transform md:-translate-x-0 md:-translate-y-0 -translate-x-1/2 -translate-y-1/2
+                    w-11/12 max-w-3xl md:px-0 text-center md:text-left
+                "
+            >
                 <h1>Bespaar slim</h1>
                 <h1>
                     Leef <span className="font-extrabold text-green-600">groen</span>
                 </h1>
                 <a href="/login">
-                    <button className="btn btn cursor-pointer rounded-xl mt-0 w-full border-none bg-white p-7 text-3xl text-black">Login/register</button>
+                    <button className="btn cursor-pointer rounded-xl p-12 md:w-auto border-none bg-white py-4 md:py-7 text-xl md:text-3xl text-black">
+                        Login/register
+                    </button>
                 </a>
             </div>
         </div>
